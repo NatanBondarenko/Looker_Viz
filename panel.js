c8709be8c0ca5e8c908aedcd2b0a8d5348ca42ba
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-12-25 10:32:41 +0200

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,26 +1,33 @@
 looker.plugins.visualizations.add({
   options: {
-    pdfFile: {
-      type: "file",
-      label: "Upload PDF File",
-    },
+    // You can add any options you need for your visualization
   },
   // Set up the initial state of the visualization
   create: function (element, config) {
     this._element = element;
+    this.renderButton();
   },
   // Render in response to the data or settings changing
   updateAsync: function (data, element, config, queryResponse, details, done) {
     // Clear any errors from previous updates
     this.clearErrors();
 
-    // Access the uploaded PDF file
-    const pdfFile = config.pdfFile;
-
-    // Display a PDF.js viewer within Looker
-    const viewerHtml = `<iframe src="path/to/pdfjs/web/viewer.html?file=${pdfFile}" width="100%" height="600px"></iframe>`;
-    element.innerHTML = viewerHtml;
+    // You can perform additional updates if needed
 
     done();
+  },
+  // Render the button
+  renderButton: function () {
+    var button = document.createElement("button");
+    button.textContent = "Open PDF to Excel Converter";
+    button.onclick = this.triggerAction.bind(this);
+
+    this._element.appendChild(button);
+  },
+  // Trigger the custom action
+  triggerAction: function () {
+    looker.plugins.sdk.ok(looker.plugins.sdk.getLookmlDashboardsApi().run_action({
+      action_id: "pdf_to_excel_action", // Replace with the actual action ID from your LookML file
+    }));
   },
 });
