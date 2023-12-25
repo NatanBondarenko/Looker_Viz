299e233ec91b1c9411f5df04fd9e88622ed4b789
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-12-25 12:42:26 +0200

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,24 +1,9 @@
 looker.plugins.visualizations.add({
-  options: {
-    menuItems: {
-      type: "array",
-      label: "Menu Items",
-      default: [
-        { label: "Section 1", target: "section1" },
-        { label: "Section 2", target: "section2" },
-        { label: "Section 3", target: "section3" },
-      ],
-    },
-  },
+  options: {},
   // Set up the initial state of the visualization
   create: function (element, config) {
-    // Create a container for the menu
-    this.menuContainer = document.createElement("div");
-    this.menuContainer.className = "custom-dashboard-menu";
-    element.appendChild(this.menuContainer);
-
-    // Add buttons to the menu based on the configured items
-    this.renderMenuItems(config.menuItems);
+    this._element = element;
+    this._createCollapsibleSections();
   },
   // Render in response to the data or settings changing
   updateAsync: function (data, element, config, queryResponse, details, done) {
@@ -29,23 +14,38 @@ looker.plugins.visualizations.add({
 
     done();
   },
-  // Function to render menu items
-  renderMenuItems: function (menuItems) {
-    // Clear existing menu items
-    this.menuContainer.innerHTML = "";
+  // Function to create collapsible sections
+  _createCollapsibleSections: function () {
+    var sections = [
+      { title: "Section 1", content: "Content for Section 1..." },
+      { title: "Section 2", content: "Content for Section 2..." },
+      // Add more sections as needed
+    ];
 
-    // Create buttons for each menu item
-    menuItems.forEach((menuItem) => {
+    sections.forEach(function (section) {
+      var collapsibleSection = document.createElement("div");
+      collapsibleSection.className = "collapsible-section";
+
       var button = document.createElement("button");
-      button.textContent = menuItem.label;
-      button.onclick = () => this.navigateToSection(menuItem.target);
-      this.menuContainer.appendChild(button);
-    });
-  },
-  // Function to navigate to a specific section
-  navigateToSection: function (target) {
-    // You can implement logic to handle navigation to the specified section
-    // For example, you might update the URL or trigger the display of a specific Looker dashboard element.
-    console.log("Navigating to section:", target);
+      button.className = "collapsible-btn";
+      button.textContent = section.title;
+
+      var content = document.createElement("div");
+      content.className = "collapsible-content";
+      content.innerHTML = "<p>" + section.content + "</p>";
+
+      button.addEventListener("click", function () {
+        if (content.style.display === "block") {
+          content.style.display = "none";
+        } else {
+          content.style.display = "block";
+        }
+      });
+
+      collapsibleSection.appendChild(button);
+      collapsibleSection.appendChild(content);
+
+      this._element.appendChild(collapsibleSection);
+    }, this);
   },
 });
