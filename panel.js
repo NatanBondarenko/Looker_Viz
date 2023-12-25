b3bcfb3f091a5b6be2c55185ad78bf084a2c1dfa
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-12-25 12:37:32 +0200

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,11 +1,24 @@
 looker.plugins.visualizations.add({
   options: {
-    // You can add any options you need for your visualization
+    menuItems: {
+      type: "array",
+      label: "Menu Items",
+      default: [
+        { label: "Section 1", target: "section1" },
+        { label: "Section 2", target: "section2" },
+        { label: "Section 3", target: "section3" },
+      ],
+    },
   },
   // Set up the initial state of the visualization
   create: function (element, config) {
-    this._element = element;
-    this.renderButton();
+    // Create a container for the menu
+    this.menuContainer = document.createElement("div");
+    this.menuContainer.className = "custom-dashboard-menu";
+    element.appendChild(this.menuContainer);
+
+    // Add buttons to the menu based on the configured items
+    this.renderMenuItems(config.menuItems);
   },
   // Render in response to the data or settings changing
   updateAsync: function (data, element, config, queryResponse, details, done) {
@@ -16,18 +29,23 @@ looker.plugins.visualizations.add({
 
     done();
   },
-  // Render the button
-  renderButton: function () {
-    var button = document.createElement("button");
-    button.textContent = "Open PDF to Excel Converter";
-    button.onclick = this.triggerAction.bind(this);
+  // Function to render menu items
+  renderMenuItems: function (menuItems) {
+    // Clear existing menu items
+    this.menuContainer.innerHTML = "";
 
-    this._element.appendChild(button);
+    // Create buttons for each menu item
+    menuItems.forEach((menuItem) => {
+      var button = document.createElement("button");
+      button.textContent = menuItem.label;
+      button.onclick = () => this.navigateToSection(menuItem.target);
+      this.menuContainer.appendChild(button);
+    });
   },
-  // Trigger the custom action
-  triggerAction: function () {
-    looker.plugins.sdk.ok(looker.plugins.sdk.getLookmlDashboardsApi().run_action({
-      action_id: "pdf_to_excel_action", // Replace with the actual action ID from your LookML file
-    }));
+  // Function to navigate to a specific section
+  navigateToSection: function (target) {
+    // You can implement logic to handle navigation to the specified section
+    // For example, you might update the URL or trigger the display of a specific Looker dashboard element.
+    console.log("Navigating to section:", target);
   },
 });
