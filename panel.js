a3f0184a894a492d218313088770d1ea442982f6
Natan Bond <97616282+NatanBondarenko@users.noreply.github.com>
2023-12-25 10:20:21 +0200

    Update panel.js

--- panel.js
+++ panel.js
@@ -1,48 +1,29 @@
 looker.plugins.visualizations.add({
   options: {
-    metricValue: {
-      type: "number",
-      label: "Metric Value",
-      default: 0,
-    },
-    metricLabel: {
-      type: "string",
-      label: "Metric Label",
-      default: "Custom Metric",
-    },
-    additionalInfo: {
-      type: "string",
-      label: "Additional Information",
-      default: "",
+    pdfFile: {
+      type: "file",
+      label: "Upload PDF File",
     },
   },
   // Set up the initial state of the visualization
   create: function (element, config) {
     this._element = element;
-    this.updateContent(config);
   },
   // Render in response to the data or settings changing
   updateAsync: function (data, element, config, queryResponse, details, done) {
     // Clear any errors from previous updates
     this.clearErrors();
 
-    // Update the content with the new configuration
-    this.updateContent(config);
+    // Access the uploaded PDF file
+    const pdfFile = config.pdfFile;
+
+    // Perform PDF data extraction (using a hypothetical function extractDataFromPDF)
+    const extractedData = extractDataFromPDF(pdfFile);
+
+    // Render the extracted data in a Looker table or other visualization
+    this._element.innerHTML = `<p>Data extracted from PDF:</p>
+      <pre>${JSON.stringify(extractedData, null, 2)}</pre>`;
 
     done();
-  },
-  // Function to update the content of the custom metric card
-  updateContent: function (config) {
-    // Create HTML content for the custom metric card
-    var htmlContent = `
-      <div style="padding: 20px; border: 1px solid #ccc; border-radius: 8px; text-align: center;">
-        <h3>${config.metricLabel}</h3>
-        <p style="font-size: 24px; font-weight: bold;">${config.metricValue}</p>
-        <p>${config.additionalInfo}</p>
-      </div>
-    `;
-
-    // Set the HTML content to the visualization element
-    this._element.innerHTML = htmlContent;
   },
 });
